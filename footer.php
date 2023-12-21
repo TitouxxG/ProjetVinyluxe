1a10f0094f2498e4f63e577e108170d20c242f5d
williampapadopoulos <152820353+williampapadopoulos@users.noreply.github.com>
2023-12-21 17:40:35 +0100

    Ajoute du header, du footer, et du CSS
    Ajoute ces 3 fichiers manquants au projet

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; 2023 Vinyluxe. Tous droits réservés.</p>
+</footer>
+
+
---
+++ header.php
@@ -0,0 +1,26 @@
+<!--    Haut de la page     -->
+<header>
+    <div style='display:flex; justify-content: space-between;'>
+        <div id="Logo" style='align-self: center;'>
+            <a href="index.html">
+                <img src="Vinyluxe_logo.png" width="175px">
+            </a>
+        </div>
+        <div style='display:flex'>
+            <div id="Searchbar">
+                <input type="search" name="uti_recherche" placeholder="Rechercher : " />
+            </div>
+            <div id="Lancer">
+                <input type="submit" value="Lancer !"/>
+            </div>
+            <a href="panier.html"><div id="Panier"> <button type="button"> Panier </button></div></a>
+            <div id="Compte"> <button type="button"> Compte </button></div>
+        </div>
+    </div>
+</header>
+    
+    <!-- Rubriques -->
+    <nav>
+        <a href="index.php">Accueil </a> |
+        <a href="produits.php" >Produits</a>  
+    </nav>
